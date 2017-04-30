1e2c1376bb33a65d72d7c746ab004fbef1a28270
tso1158687 <101104044@nccu.edu.tw>
2017-04-30 15:07:24 +0800

    Add files via upload

--- js/app-jq.js
+++ js/app-jq.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
+    // 處理畫面
     var $input = $("#searchInput").find("input");
     $input.focus(function() {
-        $input.parent().parent().parent().parent()
-            .removeClass("main")
-            .addClass("main2")
-    })
+            $input.parent().parent().parent().parent()
+                .removeClass("main")
+                .addClass("main2")
+                
+
+        })
+
+
+        // 處理mask
     var $courseMask = $("#courseDetail").find(".mask");
     var $courseContent = $("#courseDetail").find(".content").find("button")
     var $course = $("#courseDetail");
@@ -13,5 +19,14 @@ $(document).ready(function() {
     });
     $courseContent.click(function() {
         $course.fadeOut(500);
-    })
+    });
+    // 處理巡覽列
+    var $nav = $("nav").find("li")
+    $nav.click(function() {
+        $nav.removeClass("active"),
+            $(this).addClass("active"),
+            $input.parent().parent().parent().parent()
+            .removeClass("main")
+            .addClass("main2")
+    });
 });
--- js/app.controller.js
+++ js/app.controller.js
@@ -16,8 +16,10 @@
         search.input3 = "";
         search.input4 = "";
         search.input5 = "";
+        search.input6 = ""
         search.detail = "";
         search.courses = CourseSearchService.getCourses();
+        search.courses2 = search.courses;
         search.click = function() {
             console.log(search.inputex2);
             search.inputex2 = search.inputex.split(" ");
@@ -26,6 +28,7 @@
             search.input3 = search.inputex2[2];
             search.input4 = search.inputex2[3];
             search.input5 = search.inputex2[4];
+            search.input6 = search.inputex2[5]
         }
         search.delete = function() {
             search.inputex = "";
@@ -36,7 +39,10 @@
             search.input5 = "";
         }
         search.index = function($index) {
+            console.log($index-1);
+            // console.log(search.courses);
             console.log(search.courses[$index]);
+            console.log(search.courses2[$index]);
             search.detail = search.courses[$index];
             console.log(search.detail);
             angular.element(document).find('#courseDetail').fadeIn(500);
